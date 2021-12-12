cfc6fec220314f04e7df0eca0ca47a244a3c8539
chloeehaynes <cehaynes@wustl.edu>
2021-12-12 16:45:01 -0600

    added more text

--- functionality.js
+++ functionality.js
@@ -27,19 +27,19 @@ function generateStringChoice(){
             if (treble_material == CLEAR){
                 //normal tension + silverplated wound + clear nylon ==> EJ45
                 string_decison = "EJ45";
-                string_description = "";
+                string_description = "These strings aren't too rough on the hands, but still have a great sound with a lot of versatile qualities. This makes them perfect for beginners looking for a clear sound with great projection and articulation.";
                 string_link = "https://www.daddario.com/products/guitar/classical-guitar/pro-arte-nylon/ej45-pro-arte-nylon-normal-tension/";
             } else {
                 //normal tension + silverplated wound + black nylon ==> EJ49
                 string_decison = "EJ49";
-                string_description = "";
+                string_description = "These strings aren't too rough on the hands, making them great for beginners. The bass strings have a strong articulate sound, while the treble strings will provide a nice mellow tone.";
                 string_link = "https://www.daddario.com/products/guitar/classical-guitar/pro-arte-nylon/ej49-pro-arte-black-nylon-normal-tension/";
             }
         }else{
             //80/20 bronze basses
             //normal tension + 80/20 bronze wound + clear nylon ==> EJ47
             string_decison = "EJ47";
-            string_description = "";
+            string_description = "These strings aren't too rough on the hands, making them great for beginners. The bass strings are more susceptible to corrosion, but provide a really nice sound. This set also has really strong, crisp treble strings for a great, well-rounded sound.";
             string_link = "https://www.daddario.com/products/guitar/classical-guitar/pro-arte-nylon/ej47-8020-bronze-pro-arte-nylon-normal-tension/";
         }
 
@@ -50,19 +50,19 @@ function generateStringChoice(){
             if (treble_material == CLEAR){
                 //hard tension + silverplated wound + clear nylon ==> EJ46
                 string_decison = "EJ46";
-                string_description = "";
+                string_description = "These strings provide a great strong sound, perfect for live performance or recording. They do require a little more finger strength, but they are definitely manageable for the more experienced player.";
                 string_link = "https://www.daddario.com/products/guitar/classical-guitar/pro-arte-nylon/ej46-pro-arte-nylon-hard-tension/";
             } else {
                 //hard tension + silverplated wound + black nylon ==> EJ50
                 string_decison = "EJ50";
-                string_description = "";
+                string_description = "This set of strings includes really crsip, articulate bass strings alongside mellow trebles, making for a well-rounded set. They do require a little more finger strength, but they are definitely manageable for the more experienced player.";
                 string_link = "https://www.daddario.com/products/guitar/classical-guitar/pro-arte-nylon/ej50-pro-arte-black-nylon-hard-tension/";
             }
         }else{
             //80/20 bronze basses
             //hard tension + 80/20 bronze wound + clear nylon ==> EJ48
             string_decison = "EJ48";
-            string_description = "";
+            string_description = "This set of strings has nice mellow basses along with crisp trebles, to provide a great, well-rounded sound. They do require a little more finger strength, but they are definitely manageable for the more experienced player.";
             string_link = "https://www.daddario.com/products/guitar/classical-guitar/pro-arte-nylon/ej48-8020-bronze-pro-arte-nylon-hard-tension/";
         }
     }
